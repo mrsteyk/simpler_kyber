7aeae233b527b95714818ab1b7e625f5b0c86c08
Alexandr <mrsteyk1@vk.com>
2024-04-22 18:01:58 +0500

    couple undefines

--- pqclean/kyber768/clean/amalgamated.c
+++ pqclean/kyber768/clean/amalgamated.c
@@ -1,9 +1,13 @@
 // Society
 #include "../../common/fips202.c"
+#undef NROUNDS
+#undef ROL
 #include "../../common/randombytes.c"
 
 #include "./cbd.c"
 #include "./indcpa.c"
+#undef gen_a
+#undef gen_at
 #include "./kem.c"
 #include "./ntt.c"
 #include "./poly.c"
